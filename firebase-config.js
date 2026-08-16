8128bb0decab1d7774767ab989503306e6ecd501
yasinzehra2203-gif <yasinzehra2203@gmail.com>
2026-08-16 23:05:37 +0300

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzZjiwcAbQ8FJ9iYFMNH1aCaHvHY3t-xw",
+  authDomain: "giderim-cf809.firebaseapp.com",
+  projectId: "giderim-cf809",
+  storageBucket: "giderim-cf809.firebasestorage.app",
+  messagingSenderId: "698977094601",
+  appId: "1:698977094601:web:899da166b3aedea928b618",
+  measurementId: "G-0YN1XEBMSP"
+};
